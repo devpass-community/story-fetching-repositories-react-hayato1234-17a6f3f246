96c52dcbe9f2ff42a28d1611fcf85ebaeb544977
Vinicius Rocha <vinicius.workspace@gmail.com>
2023-04-24 22:17:01 -0300

    feat: add fetch repositories and show items

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
-import { ListGroup, Button, Spinner } from "react-bootstrap";
-import "./styles.css";
+import React, { useState } from 'react';
+import { ListGroup, Button, Spinner } from 'react-bootstrap';
+import './styles.css';
 
 const List = () => {
   const [repositories, setRepositories] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchRepositories = async () => {
-    // TODO
+    setIsLoading(true);
+    fetch('https://api.github.com/users/devpass-tech/repos')
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw response;
+      })
+      .then((data) => {
+        console.log(data);
+        setRepositories(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching', error);
+        setRepositories([]);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
@@ -15,15 +31,23 @@ const List = () => {
       <div className="container">
         <h2 className="title">Devpass Repositories</h2>
 
-        { isLoading ?
-        ( <Spinner/> ) : 
-        ( 
+        {isLoading ? (
+          <Spinner />
+        ) : (
           <ListGroup className="repositoriesList">
-
-          { /* TODO */ }
-          
-          </ListGroup> )}
-      <Button data-testid="button" className="button" variant="primary" onClick={() => fetchRepositories()}>Fetch repositories</Button>
+            {repositories.map((repo) => (
+              <ListGroup.Item key={repo.name}>{repo.name}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+        <Button
+          data-testid="button"
+          className="button"
+          variant="primary"
+          onClick={() => fetchRepositories()}
+        >
+          Fetch repositories
+        </Button>
       </div>
     </div>
   );
